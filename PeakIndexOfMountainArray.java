efb4ee40b34f669fcd5363da12ac7482fffc5bc0
AlexandraYu <beadsbaby@hotmail.com>
2018-08-31 12:10:35 +0800

    Update PeakIndexOfMountainArray.java
    1. make loop index start from 0
    2. edit comments

--- PeakIndexOfMountainArray.java
+++ PeakIndexOfMountainArray.java
@@ -2,11 +2,11 @@ public class PeakIndexOfMountainArray{
 
      public static int findPeakIndex(int [] A) {
          int peakIndex = 0;
-         //actually this is easy--just need to think about the bound correctly, such as if starting i with 0, then we can't compare i with i-1, b/c i-1 will be out of bound. If we start i from 1, we can avoid this problem.
-         // you can also use i fro 0, and compare i with i+1, then you need to think about the upper bound--however this probably isn't a problem in this particular case; the array is strickly a "mountain array" so the last element must not be the peak. 
-         for (int i=1; i<A.length; i++) {
-             if (A[i-1] > A[i]) {
-                 peakIndex = i-1; 
+         //actually this is easy--just need to think when the content starts to decline, which means A[i+1] < A[i], so i is the peak index 
+         //need to think about the bounds correctly--you can start i from 0, and compare i with i+1, then you need to think about the upper bound--however this probably isn't a problem in this particular case; the array is strickly a "mountain array" so the last element must not be the peak. 
+         for (int i=0; i<A.length; i++) {
+             if (A[i] > A[i+1]) {
+                 peakIndex = i; 
                  break; 
              }
          }
